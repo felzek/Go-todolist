d11e669ea61071b2218f395bcdf2a309220b6a06
felzek <yukunpengba@gmail.com>
2021-02-21 18:44:06 -0600

    add comments

--- server/models/models.go
+++ server/models/models.go
@@ -2,11 +2,15 @@ package models
 
 import "go.mongodb.org/mongo-driver/bson/primitive"
 
+// Creating a struct for ToDoList
 type ToDoList struct {
+	//Object Id that identify the task 
 	ID primitive.ObjectID `json:"_id,omitempty"
 	bson:"_id,omitempty"`
 
+	// Task name in string
 	Task string `json:"task,omitempty"`
+	// Completed status whether true or false
 	Completed bool `json:"completed,omitempty"`
 }
 
