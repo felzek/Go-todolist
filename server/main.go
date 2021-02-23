3c034de673b4af00f91a68fd59ce2145e598d019
felzek <yukunpengba@gmail.com>
2021-02-22 19:52:30 -0600

    fix docker

--- server/main.go
+++ server/main.go
@@ -1,13 +1,16 @@
 package main
+
 import (
-    "fmt"
-    "log"
-    "net/http"
-	
+	"fmt"
+	"log"
+	"net/http"
+
 	"github.com/abdennour/go-to-do-app/router"
 )
+
 func main() {
-    r := router.Router()
-    fmt.Println("server starting on 8080")
-    log.Fatal(http.ListenAndServe(":8080", r))
+	r := router.Router()
+	fmt.Println("Starting server on the port 8080...")
+
+	log.Fatal(http.ListenAndServe(":8080", r))
 }
